bdd20f79c18dc97fc7a8d2ac666056cec4c4f90a
Daulet <kutttybay@gmail.com>
2025-09-22 23:45:54 +0600

    Shop Page created

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 });
+
+
+const mainImg = document.getElementById("main-img");
+const smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = () => {
+  mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = () => {
+  mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = () => {
+  mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = () => {
+  mainImg.src = smallImg[3].src;
+}
